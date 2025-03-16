6f436b14f31dfb4d22eb768b0e2bfca4e61fc8ce
alexjqu <alex.j.qu@gmail.com>
2025-03-16 13:56:50 -0400

    button toggle (not finished)

--- src/App.js
+++ src/App.js
@@ -13,6 +13,8 @@ function SampleSizeCalculator() {
   const [power, setPower] = useState('0.8');
   const [sampleSize, setSampleSize] = useState(null);
   const [error, setError] = useState(null);
+  const [diffMean, setDiffMean] = useState('0.3'); // New state for difference in mean
+  const [allocationRatio, setAllocationRatio] = useState('1'); // New state for allocation ratio
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -27,8 +29,16 @@ function SampleSizeCalculator() {
         setType1(value);
       } else if (name === 'Power') {
         setPower(value);
+      } else if (name === 'diffMean') {
+        setDiffMean(value);
+      } else if (name === 'allocationRatio') {
+        setAllocationRatio(value);
       }
     }
+  };
+
+  const handleGroupSelection = (group) => {
+    setNumGroups(group);
   };
 
   const calculateSampleSize = () => {
@@ -91,86 +101,177 @@ function SampleSizeCalculator() {
     }
   };
 
+  const renderInputs = () => {
+    if (numGroups === 'Two (independent)') {
+      return (
+        <>
+          <label>
+            Test Objective:
+            <select value={testObjective} onChange={(e) => setTestObjective(e.target.value)}>
+              <option value={'Equality'}>Equality</option>
+              <option value={'Equivalence'}>Equivalence</option>
+              <option value={'Noninferiority'}>Noninferiority</option>
+              <option value={'Superiority'}>Superiority</option>
+            </select>
+          </label>
+          <label>
+            1 or 2 Sided Test?
+            <select value={numSides} onChange={(e) => setNumSides(e.target.value)}>
+              <option value={'1-Sided'}>1-Sided</option>
+              <option value={'2-Sided'}>2-Sided</option>
+            </select>
+          </label>
+          <label>
+            Difference in Mean
+            <input
+              type="text"
+              name="diffMean"
+              value={diffMean}
+              onChange={handleInputChange}
+              placeholder="0.3"
+            />
+          </label>
+          <label>
+            Subject Allocation Ratio
+            <input
+              type="text"
+              name="allocationRatio"
+              value={allocationRatio}
+              onChange={handleInputChange}
+              placeholder="1"
+            />
+          </label>
+          <label>
+            Type 1 Error
+            <input
+              type="text"
+              name="Type 1 Error"
+              value={type1}
+              onChange={handleInputChange}
+              placeholder="0.05"
+            />
+          </label>
+          <label>
+            Power
+            <input
+              type="text"
+              name="Power"
+              value={power}
+              onChange={handleInputChange}
+              placeholder="0.8"
+            />
+          </label>
+        </>
+      );
+    } else if (numGroups === 'One') {
+      return (
+        <>
+          <label>
+            Test Objective:
+            <select value={testObjective} onChange={(e) => setTestObjective(e.target.value)}>
+              <option value={'Equality'}>Equality</option>
+              <option value={'Equivalence'}>Equivalence</option>
+              <option value={'Noninferiority'}>Noninferiority</option>
+              <option value={'Superiority'}>Superiority</option>
+              <option value={'Correlation'}>Correlation</option>
+            </select>
+          </label>
+          <label>
+            1 or 2 Sided Test?
+            <select value={numSides} onChange={(e) => setNumSides(e.target.value)}>
+              <option value={'1-Sided'}>1-Sided</option>
+              <option value={'2-Sided'}>2-Sided</option>
+            </select>
+          </label>
+          <label>
+            Mean for Historical Control
+            <input
+              type="text"
+              name="meanHistCtrl"
+              value={meanHistCtrl}
+              onChange={handleInputChange}
+              placeholder="0.2"
+            />
+          </label>
+          <label>
+            Mean for Treatment
+            <input
+              type="text"
+              name="meanTreatment"
+              value={meanTreatment}
+              onChange={handleInputChange}
+              placeholder="0.5"
+            />
+          </label>
+          <label>
+            Standard Deviation
+            <input
+              type="text"
+              name="stdev"
+              value={stdev}
+              onChange={handleInputChange}
+              placeholder="1"
+            />
+          </label>
+          <label>
+            Type 1 Error
+            <input
+              type="text"
+              name="Type 1 Error"
+              value={type1}
+              onChange={handleInputChange}
+              placeholder="0.05"
+            />
+          </label>
+          <label>
+            Power
+            <input
+              type="text"
+              name="Power"
+              value={power}
+              onChange={handleInputChange}
+              placeholder="0.8"
+            />
+          </label>
+        </>
+      );
+    }
+  };
+
   return (
     <div className="App">
       <h1>Sample Size Calculator - Continuous Endpoint</h1>
       <div className="calculator">
         <label>
           Number of Groups:
-          <select value={numGroups} onChange={(e) => setNumGroups(e.target.value)}>
-            <option value={'One'}>One</option>
-            <option value={'Two (paired)'}>Two (paired)</option>
-            <option value={'Two (independent)'}>Two (independent)</option>
-            <option value={'>2'}>&gt;2</option>
-          </select>
+          <div className="group-buttons">
+            <button
+              className={numGroups === 'One' ? 'active' : ''}
+              onClick={() => handleGroupSelection('One')}
+            >
+              One
+            </button>
+            <button
+              className={numGroups === 'Two (paired)' ? 'active' : ''}
+              onClick={() => handleGroupSelection('Two (paired)')}
+            >
+              Two (paired)
+            </button>
+            <button
+              className={numGroups === 'Two (independent)' ? 'active' : ''}
+              onClick={() => handleGroupSelection('Two (independent)')}
+            >
+              Two (independent)
+            </button>
+            <button
+              className={numGroups === '>2' ? 'active' : ''}
+              onClick={() => handleGroupSelection('>2')}
+            >
+              &gt;2
+            </button>
+          </div>
         </label>
-        <label>
-          Test Objective:
-          <select value={testObjective} onChange={(e) => setTestObjective(e.target.value)}>
-            <option value={'Equality'}>Equality</option>
-            <option value={'Equivalence'}>Equivalence</option>
-            <option value={'Noninferiority'}>Noninferiority</option>
-            <option value={'Superiority'}>Superiority</option>
-            <option value={'Correlation'}>Correlation</option>
-          </select>
-        </label>
-        <label>
-          1 or 2 Sided Test?
-          <select value={numSides} onChange={(e) => setNumSides(e.target.value)}>
-            <option value={'1-Sided'}>1-Sided</option>
-            <option value={'2-Sided'}>2-Sided</option>
-          </select>
-        </label>
-        <label>
-          Mean for Historical Control
-          <input
-            type="text"
-            name="meanHistCtrl"
-            value={meanHistCtrl}
-            onChange={handleInputChange}
-            placeholder="0.2"
-          />
-        </label>
-        <label>
-          Mean for Treatment
-          <input
-            type="text"
-            name="meanTreatment"
-            value={meanTreatment}
-            onChange={handleInputChange}
-            placeholder="0.5"
-          />
-        </label>
-        <label>
-          Standard Deviation
-          <input
-            type="text"
-            name="stdev"
-            value={stdev}
-            onChange={handleInputChange}
-            placeholder="0.5"
-          />
-        </label>
-        <label>
-          Type 1 Error
-          <input
-            type="text"
-            name="Type 1 Error"
-            value={type1}
-            onChange={handleInputChange}
-            placeholder="0.5"
-          />
-        </label>
-        <label>
-          Power
-          <input
-            type="text"
-            name="Power"
-            value={power}
-            onChange={handleInputChange}
-            placeholder="0.8"
-          />
-        </label>
+        {renderInputs()}
         <button onClick={calculateSampleSize}>Calculate Sample Size</button>
         {sampleSize && <p>Required Sample Size: {sampleSize}</p>}
         {error && <p style={{ color: 'red' }}>{error}</p>}
